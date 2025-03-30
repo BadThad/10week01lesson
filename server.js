310236e1fa8cb19bd2de6555a80f1d31dd44df83
Peter Fänge <pellefange@gmail.com>
2025-03-30 16:45:46 +0200

    created basic list of users and have added both get and post

---
+++ server.js
@@ -0,0 +1,43 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running in http://localhost:${PORT}`);
+})
+
+let users = [
+    {
+        id: 1,
+        firstName: "Steven",
+        lastName: "Spielberg",
+    },
+    {
+        id: 2,
+        firstName: "Peter",
+        lastName: "Jackson",
+    },
+    {
+        id: 3,
+        firstName: "James",
+        lastName: "Cameron",
+    },
+];
+
+app.get("/users", (req, res) => {
+    res.json(users);
+});
+
+app.post("/users", (req, res) => {
+    const newUser = {
+        id: users.length +1,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName
+    };
+    users.push(newUser);
+    res.json({message: "New user successfully added"});
+} )
